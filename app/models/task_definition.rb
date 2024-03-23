411bad0cae58bc26f0efc36267bbb2cc88076290
satikaj <117552851+satikaj@users.noreply.github.com>
2024-03-23 19:32:59 +1100

    fix: reset Numbas configs if no zip file has been uploaded

--- app/models/task_definition.rb
+++ app/models/task_definition.rb
@@ -48,6 +48,8 @@ class TaskDefinition < ApplicationRecord
 
   validates :weighting, presence: true
 
+  after_initialize :reset_numbas_configs_if_no_zip
+
   include TaskDefinitionTiiModule
   include TaskDefinitionSimilarityModule
 
@@ -536,6 +538,8 @@ class TaskDefinition < ApplicationRecord
     if has_numbas_data?
       FileUtils.rm task_numbas_data
     end
+
+    reset_numbas_configs_if_no_zip()
   end
 
   # Get the path to the task sheet - using the current abbreviation
@@ -658,6 +662,14 @@ class TaskDefinition < ApplicationRecord
       result_with_sanitised_path
     else
       result_with_sanitised_file
+    end
+  end
+
+  def reset_numbas_configs_if_no_zip()
+    if !has_numbas_data?
+      self.has_enabled_numbas_test = false
+      self.numbas_time_delay = 'no delay'
+      self.numbas_attempt_limit = 0
     end
   end
 end
